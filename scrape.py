1fbe888d6cdedf65ed863f0880d2600609194914
muakasan <muakasan@gmail.com>
2018-02-07 19:43:51 -0600

    20 scraped pages into json

--- scrape.py
+++ scrape.py
@@ -5,12 +5,12 @@ import requests
 import json
 
 url = 'http://allrecipes.com/search/results/?wt=chocolate%20chip%20cookie&sort=re&page={}'
+recipe_prefix = 'http://allrecipes.com'
 recipe_key = '/recipe'
 
 recipe_set = set()
 
-page_num = 1
-while True:
+for page_num in range(20):
     page_url = url.format(page_num)
     response = requests.get(page_url)
     soup = BeautifulSoup(response.text, 'html5lib')
@@ -23,14 +23,9 @@ while True:
             link = article.find('a')
             if link is not None:
                 recipe_url = link['href']
-                if recipe_url.startswith(recipe_key):
+                if recipe_url.startswith(recipe_key) or recipe_url.startswith(recipe_prefix+recipe_key):
                     recipe_set.add(recipe_url)
-    else:
-        with open('test.json', 'w') as f:
-            f.write(json.dumps(list(recipe_set), sort_keys=True, indent=4, separators=(',', ': ')))
-        print('bad thing')
-    print(page_num)
-    page_num += 1
+    print("Page number {}".format(page_num))
 
 with open('test.json', 'w') as f:
     f.write(json.dumps(list(recipe_set), sort_keys=True, indent=4, separators=(',', ': ')))
