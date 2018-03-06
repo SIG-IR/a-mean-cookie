381542ca320d1f60d9b01080ccdd1196215dbc7a
Dmitri <dmitrisalov98@gmail.com>
2018-03-05 19:39:58 -0600

    Added some info to pull_recipes.py.

--- pull_recipes.py
+++ pull_recipes.py
@@ -1,5 +1,7 @@
 # Pulls all recipe websites from 'recipe_twenty_pages.json' and writes
 # their HTML to text files in the 'websites' folder.
+
+# Make sure there is a folder named 'websites' in the main directory.
 # Takes ~7 minutes for 381 recipes.
 
 import requests
@@ -17,25 +19,25 @@ startTime = datetime.now()
 # Reads in JSON from file.
 with open('recipes_twenty_pages.json') as json_file:
 	websites = json.load(json_file)
-	
+
 	# Loops over each url.
 	for url in websites:
 		# Checks if it should sleep.
 		if count % NUM_REQUESTS == 0:
 			time.sleep(TIME_TO_SLEEP)
-		
+
 		# Formats and sets the file name (Ex: 'websites/recipe083.txt')
 		countFormat = '{num:003d}'.format(num = count)
 		filename = 'websites/recipe' + countFormat + '.txt'
-		
+
 		# Gets the html for the url.
 		response = requests.get(url);
 		html = response.text
-		
+
 		# Writes the html to a file.
 		with open(filename, 'w') as outfile:
 			outfile.write(html)
-		
+
 		count += 1
 
 # Prints recipe count and execution time.
