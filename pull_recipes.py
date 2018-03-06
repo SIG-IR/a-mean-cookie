41a1ed6208d0fe89c6ddbcb1d9ed0a923f476fc8
Dmitri <dmitrisalov98@gmail.com>
2018-03-05 20:36:52 -0600

    Fixed a directory issue with pull_recipes.py.

--- pull_recipes.py
+++ pull_recipes.py
@@ -2,19 +2,24 @@
 # their HTML to text files in the 'websites' folder.
 
 # Make sure there is a folder named 'websites' in the main directory.
-# Takes ~7 minutes for 381 recipes.
+# Takes 7-10 minutes for 381 recipes.
 
 import requests
 import json
 import time
+import os
 from datetime import datetime
 
-NUM_REQUESTS = 20	# Number of requests to make before each sleep.
-TIME_TO_SLEEP = 5 	# Number of seconds to sleep between requests.
-count = 0
+NUM_REQUESTS = 20	# Number of requests to make per block.
+TIME_TO_SLEEP = 5 	# Number of seconds to sleep between request blocks.
+requestCount = 0
 
 # Used for determining execution time.
 startTime = datetime.now()
+
+# Creates a 'websites' folder if it doesn't already exist.
+if not os.path.exists('websites'):
+    os.makedirs('websites')
 
 # Reads in JSON from file.
 with open('recipes_twenty_pages.json') as json_file:
@@ -23,23 +28,23 @@ with open('recipes_twenty_pages.json') as json_file:
 	# Loops over each url.
 	for url in websites:
 		# Checks if it should sleep.
-		if count % NUM_REQUESTS == 0:
+		if (requestCount > 0) and (requestCount % NUM_REQUESTS == 0):
 			time.sleep(TIME_TO_SLEEP)
 
-		# Formats and sets the file name (Ex: 'websites/recipe083.txt')
-		countFormat = '{num:003d}'.format(num = count)
-		filename = 'websites/recipe' + countFormat + '.txt'
-
-		# Gets the html for the url.
-		response = requests.get(url);
+		# Gets the html of the website.
+		response = requests.get(url)
 		html = response.text
+
+		# Formats and sets the file name (Ex: 'websites/recipe083.txt')
+		countFormat = '{num:003d}'.format(num = requestCount)
+		filename = 'websites/recipe' + countFormat + '.txt'
 
 		# Writes the html to a file.
 		with open(filename, 'w') as outfile:
 			outfile.write(html)
 
-		count += 1
+		requestCount += 1
 
 # Prints recipe count and execution time.
 execTime = datetime.now() - startTime
-print('Wrote ' + str(count) + ' recipes in ' + str(execTime))
+print('Wrote ' + str(requestCount) + ' recipes in ' + str(execTime))
